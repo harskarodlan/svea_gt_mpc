7e15f65411036cb47c51aeda4d31f1457a27e7b8
Theresa Johansson <tessa0112@gmail.com>
2026-07-08 09:42:23 +0200

    moved NE solver into own Julia pkg for precompilation

--- src/gt_mpc/gt_mpc/controllers/game_controller.py
+++ src/gt_mpc/gt_mpc/controllers/game_controller.py
@@ -1,23 +1,13 @@
 from juliacall import Main as jl
-from pathlib import Path
 
 class GameController:
 
     def __init__(self):
-        # get current parent directory: /svea_ws/src/gt_mpc/controllers/
-        dir_path = Path(__file__).parent
-
-
-        print("loading overtake_solver.jl...")
-        # call overtake_solver.jl through julia code str eval
-        # overtake_solver.jl defines game, parameters and solver function
-        jl.seval(f'include("{dir_path}/overtake_solver.jl")')
-        print("overtake_solver.jl loaded! \nWarm up 'Platoon' compute_control...")
-
-        # dummy warm up call to compute_control to compile solve_step 
-        # (takes ~45s)
-        self.compute_control([0., 6., .5, -4., 6., .5], 'Platoon')
-        print("Warm up done!")
+        # Overtake is a Julia package (src/gt_mpc/gt_mpc/julia/Overtake),
+        # precompiled ahead of time (including a warm-up call to solve_step)
+        print("loading Overtake...")
+        jl.seval("using Overtake")
+        print("Overtake loaded!")
 
 
 
@@ -31,7 +21,7 @@ class GameController:
         :return: controls u1, u2
         """
         # compute control at this time step ut = [u1, u2]
-        ut = jl.solve_step(state, case)
+        ut = jl.Overtake.solve_step(state, case)
 
         # controls for each car ui = [acceleration, steering]
         # convert from juliacall.Float64 to regular floats
